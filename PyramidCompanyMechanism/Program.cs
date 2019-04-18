056db923ac925fa100f839d0d48db7e774ccd3f5
HP <argh413@gmail.com>
2019-04-18 16:54:04 +0430

    First Version of Program. ProgrammedBy Mohammad.H.Rabiee

---
+++ PyramidCompanyMechanism/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PyramidCompanyMechanism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> peopledic = new Dictionary<int, int>();
+            peopledic.Add(1, 0);
+            int peoplecount = 1;
+            Console.WriteLine($"Month : 0 ||| Population : 1 ||| Benefits : 0");
+            for (int month = 1; month < 18; month++)
+            {
+                int count = 0;
+                int benefits = 0;
+                for (int j = 1; j <= peoplecount; j++)
+                {
+
+                    if (peopledic[j] >= 12)
+                    {
+                        peopledic[j] += 0;
+                    }
+                    else
+                    {
+                        count++;
+                        peopledic[j] += 2;
+                    }
+                    if (peopledic[j] >= 8 || peopledic[j] >= 6)
+                    {
+                        benefits += 1;
+                    }
+
+                }
+                count *= 2;
+                for (int i = 0; i < count; i++)
+                {
+                    try
+                    {
+                        peopledic.Add(++peoplecount, 0);
+
+                    }
+                    catch
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Memory is Out !!!!!!!");
+                    }
+
+                }
+                int sum = 0;
+                foreach (var item in peopledic)
+                {
+                    sum += item.Value;
+                }
+                sum++;
+
+                Console.WriteLine($"Month : {month} ||| Population : {sum} ||| Benefits : {benefits}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,13 @@
+This is a Project that is written by C-Sharp and it shows mechanism of a pyramidcompany. 
+Description of Proj: 
+Operation of this company start with one person. every person add two people in every month. In 6th month of a person, adding people will be stopped and he\she won't add any person.
+*How many months do people of iran(population of iran: 70 milion) add to company ??
+*How much people of company will rich be ?
+
+Tip : The people will be rich that have at least 6 people in subcategory
+and be in company at least 4 months.  
+
+
+#TODO
+Optimize RAM and Process when program is running.
+....
